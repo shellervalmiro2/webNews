6a08c6c0bd3d04c3a76d937fc37035cfe6bca980
shellervalmiro2 <166161994+shellervalmiro2@users.noreply.github.com>
2026-01-09 09:57:46 +0300

    Add files via upload

---
+++ php/config/database.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "sql101.infinityfree.com");
+define("DB_USER", "if0_39722397");
+define("DB_PASS", "pehBevo9Zxfx");
+define("DB_NAME", "if0_39722397_zvelake");
+define("DB_ENC", "utf8mb3");
+?>
